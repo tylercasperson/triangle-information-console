44f4234c6b4a2a7c3d24cbc6357c131007b17276
Tyler Casperson <tylercasperson@gmail.com>
2021-01-18 23:52:57 -0700

    Able to ask questions about the sides

--- triangle-information-console/Program.cs
+++ triangle-information-console/Program.cs
@@ -5,7 +5,36 @@ namespace triangle_information_console
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            string appName = "Triangle Information in the console";
+            string appVersion = "1.0.0";
+            string appAuthor = "Tyler Casperson";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            Console.ResetColor();
+
+            string question = "What is the length of side ";
+
+            int sideAInput = 0;
+            int sideBInput = 0;
+            int sideCInput = 0;
+
+
+            Console.WriteLine(question + "A?");
+            string sideA = Console.ReadLine();
+
+            sideAInput = Int32.Parse(sideA);
+
+            Console.WriteLine(question + "B?");
+            string sideB = Console.ReadLine();
+            sideBInput = Int32.Parse(sideB);
+
+            Console.WriteLine(question + "C?");
+            string sideC = Console.ReadLine();
+            sideCInput = Int32.Parse(sideC);
+
+
             Console.WriteLine("Hello World!");
         }
     }
